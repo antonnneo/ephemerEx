ebe9ed495cd7d07f028a75827ae6d0710ea91eb7
Antonnneo <antnzmnn@gmail.com>
2023-12-23 16:34:30 +0300

    added app and db

--- app/start.py
+++ app/start.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/upload')
-def index():
+@app.route('/new')
+def new_note():
     return 'Привет, мир!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=False, port=5000, host="0.0.0.0")
